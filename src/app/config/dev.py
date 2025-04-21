aaa1d16cccf8240e1333d936483ab6eeeca7d93e
illuminateP <sincelife777@gmail.com>
2025-04-21 18:43:18 +0900

    modify dev.py for GCP env

--- src/app/config/dev.py
+++ src/app/config/dev.py
@@ -2,3 +2,10 @@ import os
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 DATABASE_URL = os.getenv("DB_URL")
+
+DB_HOST = os.getenv("DB_HOST")  
+DB_PORT = os.getenv("DB_PORT")           
+DB_USER = os.getenv("DB_USER")   
+DB_PASSWORD = os.getenv("DB_PASSWORD")  
+DB_NAME = os.getenv("DB_NAME")   
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")  
