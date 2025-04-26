be7917ff2c1d5fe575f8db7e88c9ce82c66941b2
loading1031 <tjdans1031@gmail.com>
2025-04-26 14:48:50 +0900

    chore: 로컬 개발환경 세팅 - docker 컴포즈화 - 볼륨 마운트로 로컬은 COPY 안함 - 환경변수 로그 추가

--- src/app/config/__init__.py
+++ src/app/config/__init__.py
@@ -5,5 +5,6 @@ profile = os.getenv("PROFILE", "local")  # default = local
 
 # config.local 또는 config.dev 로딩
 config_module = importlib.import_module(f".{profile}", package=__name__)
+print("✅ config_module:", dir(config_module))  # 추가!
 
 config = config_module
